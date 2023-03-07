cc7379d08768de7575b543192bf2dd2c518b62e1
Aleksey Tyurnin <turlex251@gmail.com>
2023-03-07 10:05:21 +0300

    Initial Commit

---
+++ MyCalendar/MyCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalendarApp.swift
+//  MyCalendar
+//
+//  Created by Aleksei Tiurnin on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
