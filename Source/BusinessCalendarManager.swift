af39b2ec770906e832f03a0268301b0ab60138de
Aleksey Tyurnin <turlex251@gmail.com>
2023-04-05 15:22:17 +0300

    перенес

---
+++ Source/BusinessCalendarManager.swift
@@ -0,0 +1,94 @@
+//
+//  BusinessCalendarManager.swift
+//  WallCall
+//
+//  Created by Aleksei Tiurnin on 05.04.2023.
+//  Copyright © 2023 Redmadrobot OOO. All rights reserved.
+//
+
+import Apexy
+import ApexyURLSession
+import Foundation
+
+struct BusinessCalendar: Codable {
+    let holidays: [Date]
+    let preholidays: [Date]
+}
+
+// https://raw.githubusercontent.com/d10xa/holidays-calendar/master/json/consultant2023.json
+
+private class BusinessCalendarNetworkClient {
+    static let decoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        return decoder
+    }()
+    static let encoder: JSONEncoder = {
+        let encoder = JSONEncoder()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY-MM-dd"
+        encoder.dateEncodingStrategy = .formatted(formatter)
+        return encoder
+    }()
+    
+    private let client: Client = {
+        let requestAdapter = BaseRequestAdapter(baseURL: URL(string: "https://raw.githubusercontent.com/")!)
+        let client = URLSessionClient(requestAdapter: requestAdapter, configuration: .default)
+        return client
+    }()
+
+    struct BusinessCalendarEndpoint: Endpoint, URLRequestBuildable {
+        typealias Content = BusinessCalendar
+        
+        let year: Int
+        
+        func makeRequest() throws -> URLRequest {
+            get(URL(string: "d10xa/holidays-calendar/master/json/consultant\(year).json")!, queryItems: nil)
+        }
+        
+        func content(from response: URLResponse?, with body: Data) throws -> Content {
+            return try decoder.decode(Content.self, from: body)
+        }
+    }
+    
+    func getCalendar(for year: Int) async throws -> BusinessCalendar {
+        return try await client.request(BusinessCalendarEndpoint(year: year))
+    }
+}
+
+class BusinessCalendarManager {
+    
+    private let client = BusinessCalendarNetworkClient()
+    private var businessCalendars: [Int: BusinessCalendar] = [:]
+    private let storage: UserDefaults = UserDefaults.standard
+    
+    func load(for date: Date) async {
+        do {
+            let year = calendar.component(.year, from: date)
+            let calendar = try await client.getCalendar(for: year)
+            businessCalendars[year] = calendar
+            save(calendar: calendar, for: year)
+        } catch {
+        }
+    }
+    
+    func getBusinessCalendar(for date: Date) -> BusinessCalendar? {
+        let year = calendar.component(.year, from: date)
+        return businessCalendars[year] ?? loadCalendar(for: year)
+    }
+    
+    func save(calendar: BusinessCalendar, for year: Int) {
+        let data = try? BusinessCalendarNetworkClient.encoder.encode(calendar)
+        storage.set(data, forKey: "\(year)")
+    }
+    
+    func loadCalendar(for year: Int) -> BusinessCalendar? {
+        guard let data = storage.data(forKey: "\(year)"),
+              let calendar = try? BusinessCalendarNetworkClient.decoder.decode(BusinessCalendar.self, from: data) else {
+            return nil
+        }
+        return calendar
+    }
+}
--- Source/MyCalendarApp.swift
+++ Source/MyCalendarApp.swift
@@ -5,49 +5,7 @@
 //  Created by Aleksei Tiurnin on 07.03.2023.
 //
 
-import Apexy
-import ApexyURLSession
 import SwiftUI
-
-// https://raw.githubusercontent.com/d10xa/holidays-calendar/master/json/consultant2023.json
-
-let client: Client = {
-    let requestAdapter = BaseRequestAdapter(baseURL: URL(string: "https://raw.githubusercontent.com/")!)
-    let client = URLSessionClient(requestAdapter: requestAdapter, configuration: .default)
-    return client
-}()
-
-let bcDateFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "YYYY-MM-dd"
-    return formatter
-}()
-
-let decoder: JSONDecoder = {
-   let decoder = JSONDecoder()
-    decoder.dateDecodingStrategy = .formatted(bcDateFormatter)
-    return decoder
-}()
-
-let encoder: JSONEncoder = {
-    let encoder = JSONEncoder()
-    encoder.dateEncodingStrategy = .formatted(bcDateFormatter)
-     return encoder
-}()
-
-struct BusinessCalendarEndpoint: Endpoint, URLRequestBuildable {
-    typealias Content = BusinessCalendar
-    
-    let year: Int
-    
-    func makeRequest() throws -> URLRequest {
-        get(URL(string: "d10xa/holidays-calendar/master/json/consultant\(year).json")!, queryItems: nil)
-    }
-    
-    func content(from response: URLResponse?, with body: Data) throws -> Content {
-        return try decoder.decode(Content.self, from: body)
-    }
-}
 
 @main
 struct MyCalendarApp: App {
