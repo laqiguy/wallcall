aac29f8daac5d2107c3b248b44792f22b650098a
Aleksey Tyurnin <turlex251@gmail.com>
2023-04-05 18:16:52 +0300

    поправил цвет по умолчанию

--- Source/ViewModels/TextViewModel.swift
+++ Source/ViewModels/TextViewModel.swift
@@ -14,7 +14,7 @@ extension Color {
 struct TextViewModel {
     var colors: [Color] = [.white, .black, .redMadColor]
     var textColor: Color = .white
-    var dayoffColor: Color = .red
+    var dayoffColor: Color = .white
     var shadowColor: Color = .black
     var isWhite: Bool = true
     var family: String = "CoFo Redmadrobot"
